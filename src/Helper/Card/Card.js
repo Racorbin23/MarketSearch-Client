aadd4145359b7436b89552993e82996cd4f73066
RANDEL CORBIN <randelcorbin@gmail.com>
2022-04-09 19:28:20 -0400

    Working css for opened card - not sorted/organized

--- src/Helper/Card/Card.js
+++ src/Helper/Card/Card.js
@@ -1,36 +1,94 @@
+import Reace, { useState } from "react";
 import "./Card.css";
 
+import CloseIcon from "../../images/close-icon.svg";
+
 function Card({ card }) {
+  const [open, setOpen] = useState(false);
+  if (open) {
+    return <OpenedCard card={card} setOpen={setOpen} />;
+  } else {
+    return <ClosedCard card={card} setOpen={setOpen} />;
+  }
+}
+
+function OpenedCard({ card, setOpen }) {
   return (
-    <div className="card-wrapper">
+    <div className="card-opened-wrapper">
+      <div className="card-opened-top">
+        <CardImage card={card} />
+        <div className="card-opened-info">
+          <CardInfo card={card} opened={true} />
+
+          <div className="card-opened-prices">
+            <CardPrice auctions={card.ps_auctions} system={"PSN"} />
+            <CardPrice auctions={card.xbox_auctions} system={"XBOX"} />
+          </div>
+        </div>
+        <CardClose setOpen={setOpen} />
+      </div>
+
+      <div>GRAPH HERE</div>
+    </div>
+  );
+}
+
+function ClosedCard({ card, setOpen }) {
+  return (
+    <div className="card-wrapper" onClick={() => setOpen(true)}>
       <CardImage card={card} className="player-img" />
       <CardInfo card={card} className="card-info" />
     </div>
   );
 }
 
-function CardInfo({ card }) {
+function CardClose({ setOpen }) {
   return (
-    <div className="card-info-wrapper">
-      <div className="card-info">
-        <div className="card-info-playername">{card.player_name}</div>
-        <div className="card-info-collectionname">{card.collection_name}</div>
-        <div className="card-info-tabname">{card.tab_name}</div>
-      </div>
-      <div className="card-info-prices-wrapper">
-        <CardPrice
-          className="card-info-prices"
-          auctions={card.xbox_auctions}
-          system={"XBOX"}
-        />
-        <CardPrice
-          className="card-info-prices"
-          auctions={card.ps_auctions}
-          system={"PSN"}
-        />
-      </div>
-    </div>
+    <img
+      className="card-opened-close"
+      src={CloseIcon}
+      alt={"Close"}
+      onClick={() => setOpen(false)}
+    />
   );
+}
+
+function CardInfo({ card, opened }) {
+  if (opened) {
+    return (
+      <div className="card-opened-info-wrapper">
+        <div className="card-opened-info">
+          <div className="card-opened-info-playername">{card.player_name}</div>
+          <div className="card-opened-info-collectionname">
+            {card.collection_name}
+          </div>
+          <div className="card-opened-info-tabname">{card.tab_name}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card-info-wrapper">
+        <div className="card-info">
+          <div className="card-info-playername">{card.player_name}</div>
+          <div className="card-info-collectionname">{card.collection_name}</div>
+          <div className="card-info-tabname">{card.tab_name}</div>
+        </div>
+        <div className="card-info-prices-wrapper">
+          <CardPrice
+            className="card-info-prices"
+            auctions={card.xbox_auctions}
+            system={"XBOX"}
+          />
+          <CardPrice
+            className="card-info-prices"
+            auctions={card.ps_auctions}
+            system={"PSN"}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 function CardPrice({ auctions, system }) {
