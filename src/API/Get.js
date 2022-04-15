812cf8a864efe3b4e7e260dfa122d7fb540f961d
RANDEL CORBIN <randelcorbin@gmail.com>
2022-04-15 02:34:41 -0400

    Updated for live deployment

--- src/API/Get.js
+++ src/API/Get.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const IP = "192.168.1.25";
-const PORT = "3001";
-const URL = `http://${IP}:${PORT}/`;
+if (process.env.NODE_ENV === "development") {
+  const IP = "192.168.1.25";
+  const PORT = "3001";
+  const URL = `http://${IP}:${PORT}/`;
+} else {
+  const URL = "https://infinite-mesa-46973.herokuapp.com/";
+}
 
 function GetAuctions(setData) {
   axios.get(URL + "cards/getAuctions").then((res) => {
